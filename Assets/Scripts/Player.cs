2f3ec98b6c3a1bc15162aa9589c855285421701d
sergoutback <webart2013@gmail.com>
2023-05-01 01:11:27 +0300

    Add bool canMove logics
    pause at 2:17:00
    
    Add  RaycastHit which is commented out right now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,16 +11,74 @@ public class Player : MonoBehaviour
     private GameInput gameInput;
 
     private bool isWalking;
+
+    // private LineRenderer lineRenderer;
+    
+    // void Start()
+    // {
+    //     lineRenderer = gameObject.AddComponent<LineRenderer>();
+    //     lineRenderer.material = Resources.Load<Material>("RaycastLineMaterial");
+    //     lineRenderer.startWidth = 0.1f;
+    //     lineRenderer.endWidth = 0.1f;
+    // }
+
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovingVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        //RaycastHit hit;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHight = 2f;
+        //bool canMove = !Physics.Raycast(transform.position, moveDir, out hit, playerSize);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDir, moveDistance);
 
+        if (!canMove)
+        {
+            //Cannot move towards moveDir
+            
+            //Attempt only x movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirX, moveDistance);
+            
+            if (canMove)
+            {
+                //Can move only the x
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //Cannot move only the x
+            
+                //Attempt only z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    //Can move only the z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot move in any direction
+                }
+
+            }
+        }
+            
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
         isWalking = moveDir != Vector3.zero;
         
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward ,moveDir, Time.deltaTime * rotateSpeed);
+        // Отображение луча Raycast в виде линии
+        // lineRenderer.SetPosition(0, transform.position);
+        // lineRenderer.SetPosition(1, transform.position + moveDir * playerSize);
     }
 
     public bool IsWalking()
