c6f90887d49000706ef18f2e51439a9cf5c92d3d
sergoutback <webart2013@gmail.com>
2023-05-02 01:44:38 +0300

    Add SelectedCounterVisual (singleton)
    pause at 3:10:41

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+    
     [SerializeField]
     private float moveSpeed = 7f;
     
@@ -15,10 +23,20 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
 
     // private LineRenderer lineRenderer;
-    
-     void Start()
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("There is more than one Player instance.");
+        }
+        Instance = this;
+    }
+
+    private void Start()
      {
          gameInput.OnInteractAction += GameInput_OnInteractAction;
          //     lineRenderer = gameObject.AddComponent<LineRenderer>();
@@ -29,20 +47,9 @@ public class Player : MonoBehaviour
 
      private void GameInput_OnInteractAction(object sender, EventArgs e)
      {
-         float interactDistance = 2f;
-         Vector2 inputVector = gameInput.GetMovingVectorNormalized();
-         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-         if (moveDir != Vector3.zero)
+         if (selectedCounter != null)
          {
-             lastInteractDir = moveDir;
-         }
-         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance))
-         {
-             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-             {
-                 //Has ClearCounter
-                 clearCounter.Interact();
-             }
+             selectedCounter.Interact();
          }
      }
 
@@ -72,7 +79,19 @@ public class Player : MonoBehaviour
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //Has ClearCounter
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
             }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
         }
     }
 
@@ -133,5 +152,15 @@ public class Player : MonoBehaviour
         // Отображение луча Raycast в виде линии
         // lineRenderer.SetPosition(0, transform.position);
         // lineRenderer.SetPosition(1, transform.position + moveDir * playerSize);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = selectedCounter
+        });
     }
 }
